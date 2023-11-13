8247c842fa023ea61ef806baa2ff772afe2952d8
MahmoudEl-Husseni <oudm753@gmail.com>
2023-11-13 20:41:23 +0200

    Lane Features Added, Agent & Object Features Modified

--- Argoverse2/rawTOinterm.py
+++ Argoverse2/rawTOinterm.py
@@ -1,5 +1,7 @@
 from config import *
-from utils import normalize, get_interpolated_xy
+from utils.geometry import calc_direction, n_candidates, Angle_Distance_from_agent
+
+from Argoverse2.utils.visualize import normalize, get_interpolated_xy
 
 
 import os
@@ -8,6 +10,8 @@ import argparse
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
+from pathlib import Path
+
 
 sys.path.append("/content/av2-api/src")
 
@@ -34,73 +38,167 @@ def extract_agent_features(loader):
   agent_df = df[df['track_id']==track_id_]
   cur_df = df[df['timestep']==N_PAST-1]
 
-
+  # Past XY
   vec = agent_df[agent_df['timestep']<N_PAST][['position_x', 'position_y']].values
   center = cur_df.loc[cur_df['track_id']==track_id_, ['position_x', 'position_y']].values.reshape(-1)
 
   XY_past = normalize(vec, center)
 
-  agent_type = np.zeros(N_PAST)
-  timesteps = agent_df['timestep']
-  track_id = np.ones(N_PAST) * int(agent_df.iloc[0]['track_id'])
+  # Timestep
+  timesteps = agent_df['timestep'].values
 
+  # Vx, Vy
+  Vx = agent_df['velocity_x'].values
+  Vy = agent_df['velocity_y'].values
+
+  # Heading
+  heading = agent_df['heading'].values
+
+  # Candidates Denisity
   fl_points = agent_df.sort_values(by='timestep').iloc[[0, N_PAST]].loc[:, ['position_x', 'position_y']].values
   radius = np.linalg.norm(fl_points[1]-fl_points[0])
+  CD = n_candidates(df, loader, radius * RADIUS_OFFSET)
 
-  cur_df['displacement'] = cur_df.apply(lambda x : np.linalg.norm((x['position_x'] - center[0], x['position_y'] - center[1])), axis=1)
-  cur_df['is_candidate'] = (cur_df['displacement'] <= (RADIUS_OFFSET*radius)) & (cur_df['track_id']!=track_id_)
-  candidate_ids = cur_df.loc[cur_df['is_candidate'], 'track_id'].values
-  
+  # Agent type
+  agent_type = np.zeros(N_PAST)
+
+  # Track ID
+  track_id = np.ones(N_PAST) * int(agent_df.iloc[0]['track_id'])
+
+
+  # cur_df['displacement'] = cur_df.apply(lambda x : np.linalg.norm((x['position_x'] - center[0], x['position_y'] - center[1])), axis=1)
+  # cur_df['is_candidate'] = (cur_df['displacement'] <= (RADIUS_OFFSET*radius)) & (cur_df['track_id']!=track_id_)
+  # candidate_ids = cur_df.loc[cur_df['is_candidate'], 'track_id'].values
+
+  # Ground Truth Trajectory
   gt = agent_df.loc[agent_df['timestep']>=N_PAST, ['position_x', 'position_y']].values
+
+  # Normalized GT
   gt_normalized = normalize(gt, center)
 
   data = {
-      'XY_past' : XY_past, 
-      'agent_type' : agent_type, 
-      'timesteps' : timesteps, 
-      'track_id' : track_id, 
-      'candidate_ids' : candidate_ids, 
-      'gt' : gt, 
-      'gt_normalized' : gt_normalized 
+      'XY_past' : XY_past,
+      'timesteps' : timesteps,
+      'vx' : Vx, 
+      'vy' : Vy, 
+      'heading' : heading, 
+      'candidate_density' : CD,
+      'agent_type' : agent_type,
+      'track_id' : track_id,
+      # 'candidate_ids' : candidate_ids,
+      'gt' : gt,
+      'gt_normalized' : gt_normalized
   }
 
-  return data 
+  return data, center, radius
 
 
-def extract_obj_features(df, focal_track_id):
+
+def extract_obj_features(df, loader):
+  
+  focal_track_id = loader.focal_track_id
 
   norm_vec = df.loc[(df['track_id']==focal_track_id) & (df['timestep']==N_PAST), ['position_x', 'position_y']].values.reshape(-1)
   obj_track_ids = df.loc[df['track_id']!=focal_track_id, 'track_id'].unique()
-  
+
   XYTs = np.empty((0, 5))
-  object_types = np.empty((0, 1))
+  DIST = np.empty((0, 1))
+  ANGLE = np.empty((0, 1))
+  HEADING = np.empty((0, 1))
+  VELOCITY = np.empty((0, 2))
+  OBJECT_TYPE = []
+  
   mask_tovectors = []
   end = 0
+  p_id = 0
 
-  for t_id in obj_track_ids: 
+  for t_id in obj_track_ids:
+
+    Angle_Distance_from_agent(df, loader)
     obj_df = df[df['track_id']==t_id]
-    t = obj_df['timestep']
-    yx = obj_df['position_x']
-    yy = obj_df['position_y']
+
+    # XY_past
+    t = obj_df['timestep'].values
+    yx = obj_df['position_x'].values
+    yy = obj_df['position_y'].values
 
     yx_, yy_ = get_interpolated_xy(t, yx, yy)
     yx_norm = normalize(yx_, norm_vec[0]).reshape(-1, 1)
     yy_norm = normalize(yy_, norm_vec[1]).reshape(-1, 1)
-    
+
+    # Distance From Agent
+    distance = obj_df['displacement_from_agent'].values.reshape(-1, 1)
+    angle = obj_df['angle_to_agent'].values.reshape(-1, 1)
+
+    # Heading
+    heading = obj_df['heading'].values.reshape(-1, 1)
+
+    # velocity
+    vx = obj_df['velocity_x'].values.reshape(-1, 1)
+    vy = obj_df['velocity_y'].values.reshape(-1, 1)
+
+    # Polyline id
+    polyline_id = p_id
+
+    # timestep
     timesteps = np.arange(t.min(), t.max()+1)
-    object_type = obj_df['object_type'].iloc[0]
+    
+    # Object type
+    OBJECT_TYPE.append([obj_df['object_type'].iloc[0]] * len(timesteps))
 
     XYTs = np.vstack((XYTs, np.hstack((yx_.reshape(-1, 1), yy_.reshape(-1, 1), yx_norm, yy_norm, timesteps.reshape(-1, 1)))))
-    mask_tovectors.append(slice(end, end+len(yx_)))  
+    DIST = np.vstack((DIST, distance))
+    ANGLE = np.vstack((angle, angle))
+    HEADING = np.vstack((HEADING, heading))
+    VELOCITY = np.vstack((VELOCITY, np.hstack((vx, vy))))
+
+    mask_tovectors.append(slice(end, end+len(yx_)))
     end += len(XYTs)
 
+    p_id += 1
   data = {
-      'XYTs' : XYTs, 
-      'object_type' : object_type, 
+      'XYTs' : XYTs,
+      'DIST' : DIST, 
+      'ANGLE' : ANGLE, 
+      'HEADING' : HEADING, 
+      'VELOCITY' : VELOCITY,
+      'object_type' : OBJECT_TYPE,
       'mask_tovectors' : mask_tovectors
   }
   return data
 
+
+def extract_lane_features(avm, center, radius):
+  polylines = avm.get_nearby_lane_segments(center, radius*RADIUS_OFFSET)
+  XYZ = []
+  IS_INTER = []
+  TYP = []
+  ID = []
+  DIRECTION = []
+
+  for poly in polylines:
+    xyz = poly.polygon_boundary
+    is_inter = poly.is_intersection
+    typ = poly.lane_type
+    id = poly.id
+    dir = calc_direction(xyz)
+
+
+    XYZ.append(xyz)
+    IS_INTER.append(is_inter)
+    TYP.append(typ)
+    ID.append(id)
+    DIRECTION.append(dir)
+    
+  data = {
+      "XYZ" : XYZ,
+      "IS_INTER" : IS_INTER,
+      "TYP" : TYP,
+      "ID" : ID, 
+      "DIRECTION" : DIRECTION
+  }
+
+  return data
 
 def main():
     args = argparser()
@@ -115,9 +213,15 @@ def main():
     for scene in tqdm(SCENE_DIRS): 
         file_name = "scenario_" + scene.split('/')[-1] + ".parquet"
         loader = ss.load_argoverse_scenario_parquet(file_name)
-        agent_data = extract_agent_features(loader)
-        obj_data = extract_obj_features(loader.df, loader.focal_track_id)
+        
+        log_map_dirpath = Path(scene)
+        avm = ArgoverseStaticMap.from_map_dir(log_map_dirpath=log_map_dirpath, build_raster=False)
 
+
+        agent_data, center, radius = extract_agent_features(loader)
+        obj_data = extract_obj_features(loader.df, loader.focal_track_id)
+        lane_data = extract_lane_features(avm, center, radius)
+        
         np.savez(os.path.join(SAVE_DIR, scene.split('/')[-1] + "_agent"), **agent_data)
         np.savez(os.path.join(SAVE_DIR, scene.split('/')[-1] + "_obj"), **obj_data)
 
---
+++ Argoverse2/utils/geometry.py
@@ -0,0 +1,70 @@
+import numpy as np
+from scipy.interpolate import UnivariateSpline
+
+def normalize(vector, point): 
+  return vector - point
+
+def get_interpolated_xy(timesteps, x_coord, y_coord):
+  n_ts = (timesteps.max() - timesteps.min() + 1)
+  x = timesteps
+  yx = x_coord
+  yy = y_coord
+  if n_ts > len(x):
+    interp_func_X = UnivariateSpline(x, yx)
+    interp_func_Y = UnivariateSpline(x, yy)
+
+    yx_ = []
+    yy_ = []
+    it = 0
+    for i in range(x.min(), x.max()+1):
+      if i not in x:
+        yx_.append(interp_func_X(i))
+        yy_.append(interp_func_Y(i))
+      else:
+        yx_.append(yx[it])
+        yy_.append(yy[it])
+        it+=1
+  else :
+    return yx, yy
+
+  return np.array(yx_), np.array(yy_)
+
+def Angle_Distance_from_agent(df, loader):
+  track_id = loader.focal_track_id
+  positions = df.loc[df['track_id']==track_id, ['position_x', 'position_y']].values
+  t_ids = df.loc[df['track_id']!=track_id, 'track_id'].unique()
+  df['displacement_from_agent'] = np.zeros(len(df))
+  df['angle_to_agent'] = np.zeros(len(df))
+  
+  for id in t_ids:
+    dd = df.loc[df['track_id']==id]
+    t = dd['timestep'].values
+    agent_p = positions[t - t.min()]
+    diff = agent_p - dd[['position_x', 'position_y']].values
+
+    angles = np.arctan(diff[:, 1] / diff[:, 0])
+    dd['angle_to_agent'] = angles
+
+    disp = np.linalg.norm(diff, axis=1)
+    dd['displacement_from_agent'] = disp
+
+    df[df['track_id']==id] = dd.values
+
+def n_candidates(df, loader, distance):
+  Angle_Distance_from_agent(df, loader)
+  df['is_candidate'] = df['displacement_from_agent'].apply(lambda x : x <= distance)
+
+  return df.groupby('timestep')['is_candidate'].sum().values
+
+
+def calc_direction(xyz):
+  direction = []
+  lastx, lasty, lastz = xyz[0]
+  for d in xyz[1:]: 
+    x, y, z = d
+    dir = (y - lasty) / (x - lastx)
+    lastx, lasty, lastz = [x, y, z]
+    direction.append(dir)
+
+  direction.append(dir)
+  return np.array(direction)
